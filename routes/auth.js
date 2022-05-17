7b65af20088f71942d3cba9f5ebdaa7b6e2b1a0f
Rahul <rahulkottikasala@gmail.com>
2022-05-17 14:27:46 +0400

    login auth as success

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJS = require("crypto-js")
+const CryptoJS = require("crypto-js");
 
 //REGISTER
-
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -16,9 +15,29 @@ router.post("/register", async (req, res) => {
     res.status(201).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
-
   }
 });
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials");
 
-module.exports  = router;
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials");
+
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
